783429a60f53c434a28cde3353d584b9098075fd
Vitalik Pasemko <88082270+VitalikPasemko@users.noreply.github.com>
2021-09-22 19:42:05 +0300

    Create day-03.js

---
+++ day-03.js
@@ -0,0 +1,42 @@
+//https://www.codewars.com/kata/571f832f07363d295d001ba8/train/javascript
+function trueOrFalse(val) {
+  if (val) return "true";
+  return "false";
+}
+
+//https://www.codewars.com/kata/57202aefe8d6c514300001fd/train/javascript
+function saleHotdogs(n){
+  if (n>=10) return n * 90;
+  else if (n<5) return n * 100;
+  else if (n<10) return n * 95;
+}
+
+function saleHotdogs(n){
+  return (n<5) ? n * 100 : (n<10) ? n * 95 : n * 90;
+}
+
+//https://www.codewars.com/kata/572059afc2f4612825000d8a/train/javascript
+function howManydays(month) {
+  let days;
+  switch (month) {
+    case 2:
+      days = 28;
+      break;
+    case 4:
+      days = 30;
+      break;
+    case 6:
+      days = 30;
+      break;
+    case 9:
+      days = 30;
+      break;
+    case 11:
+      days = 30;
+      break;
+    default:
+      days = 31;
+      break;
+  }
+  return days;
+}
