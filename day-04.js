0d56dd5f948c1f76347e7464bb07cb6928d27ee7
Vitalik Pasemko <88082270+VitalikPasemko@users.noreply.github.com>
2021-09-26 12:56:59 +0300

    Update day-04.js

--- day-04.js
+++ day-04.js
@@ -15,3 +15,29 @@ function pickIt(arr) {
   }
   return [odd, even];
 }
+
+//https://www.codewars.com/kata/5721c189cdd71194c1000b9b/train/javascript
+function grabDoll(dolls) {
+  var bag = [];
+  for (var i = 0; i < dolls.length; i++) {
+    if (dolls[i] === "Hello Kitty" || dolls[i] === "Barbie doll")
+      bag.push(dolls[i]);
+    else continue;
+    if (bag.length === 3) break;
+  }
+  return bag;
+}
+
+
+//https://www.codewars.com/kata/5722b3f0bd5583cf44001000/train/javascript
+function giveMeFive(obj) {
+  var five = [];
+  for (var key in obj) {
+    if (key.length == 5) 
+      five.push(key);
+    if (obj[key].length == 5)
+      five.push(obj[key]);
+  }
+  return five;
+}
+}
