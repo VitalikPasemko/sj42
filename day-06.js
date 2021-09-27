51c9cb53506ae96e2a90173e7f55863502e48d52
Vitalik Pasemko <88082270+VitalikPasemko@users.noreply.github.com>
2021-09-27 18:25:37 +0300

    Update day-06.js

--- day-06.js
+++ day-06.js
@@ -2,4 +2,9 @@
 function unusualFive() {
   return 'value'.length;
 }
+//https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
+function hero(bullets, dragons){
+  return dragons * 2 <= bullets;
+}
+
 
