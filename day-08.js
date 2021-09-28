3a91615fd64eddeb2e5802f35f8b841d9dd0c133
Vitalik Pasemko <88082270+VitalikPasemko@users.noreply.github.com>
2021-09-28 20:39:03 +0300

    Update day-08.js

--- day-08.js
+++ day-08.js
@@ -7,3 +7,15 @@ function whatNumberIsIt(n){
   if (Number(n)) return `Input number is ${n}`;
   else return `Input number is Number.NaN`;
 }
+
+//https://www.codewars.com/kata/57238ceaef9008adc7000603/train/javascript
+function colorOf(r, g, b) {
+  let colorRed = r.toString(16);
+  let colorGreen = g.toString(16);
+  let colorBlue = b.toString(16);
+
+  colorRed = colorRed.length < 2 ? "0" + colorRed : colorRed;
+  colorGreen = colorGreen.length < 2 ? "0" + colorGreen : colorGreen;
+  colorBlue = colorBlue.length < 2 ? "0" + colorBlue : colorBlue;
+  return "#" + colorRed + colorGreen + colorBlue;
+}
