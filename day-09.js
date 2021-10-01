0a889f9f6375353352295e8ae31c96dfad805106
Vitalik Pasemko <88082270+VitalikPasemko@users.noreply.github.com>
2021-10-02 00:36:32 +0300

    Update day-09.js

--- day-09.js
+++ day-09.js
@@ -18,5 +18,9 @@ function firstToLast(str,c){
 }
 //https://www.codewars.com/kata/57280481e8118511f7000ffa/train/javascript
 function splitAndMerge(string, separator) {
-  return string.split('').join(separator + '').split(separator + ' ' + separator).join(' ');
+  return string
+    .split("")
+    .join(separator + "")
+    .split(separator + " " + separator)
+    .join(" ");
 }
