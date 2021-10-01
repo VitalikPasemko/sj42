ff280978a576f6a4885ca4dbaa00d5fe7d1804aa
Vitalik Pasemko <88082270+VitalikPasemko@users.noreply.github.com>
2021-10-01 15:06:20 +0300

    Update day-09.js

--- day-09.js
+++ day-09.js
@@ -1,1 +1,13 @@
-//
+//https://www.codewars.com/kata/57274562c8dcebe77e001012/train/javascript
+function cutIt(arr){
+  let minlength = Infinity;
+  for (let i=0; i<arr.length; i++) {
+    if (arr[i].length < minlength) {
+      minlength = arr[i].length;
+    }
+  }
+  for (let i=0; i<arr.length; i++) {
+    arr[i] = arr[i].slice(0,minlength);
+  }
+  return arr;
+}
