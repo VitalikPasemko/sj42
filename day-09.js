bd622c133f495556f1417a54f2f719623cd8cdae
Vitalik Pasemko <88082270+VitalikPasemko@users.noreply.github.com>
2021-10-01 23:00:04 +0300

    Update day-09.js

--- day-09.js
+++ day-09.js
@@ -1,13 +1,18 @@
 //https://www.codewars.com/kata/57274562c8dcebe77e001012/train/javascript
-function cutIt(arr){
+function cutIt(arr) {
   let minlength = Infinity;
-  for (let i=0; i<arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     if (arr[i].length < minlength) {
       minlength = arr[i].length;
     }
   }
-  for (let i=0; i<arr.length; i++) {
-    arr[i] = arr[i].slice(0,minlength);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].slice(0, minlength);
   }
   return arr;
+}
+
+//https://www.codewars.com/kata/57277a31e5e51450a4000010/train/javascript
+function firstToLast(str,c){
+  return str.includes(c) ? str.lastIndexOf(c) - str.indexOf(c) : -1;
 }
