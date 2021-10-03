fa0b7e97bf0dc617ac3bd1756caba256ea36a635
Vitalik Pasemko <88082270+VitalikPasemko@users.noreply.github.com>
2021-10-04 00:04:34 +0300

    Create day-10.js

---
+++ day-10.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/5728203b7fc662a4c4000ef3/train/javascript
+function alienLanguage(str) {
+  return str
+    .split(" ")
+    .map((v) => v.slice(0, -1).toUpperCase() + v.slice(-1).toLowerCase())
+    .join(" ");
+}
