b522778a4bafbab66c8216cf08af21a9ec397528
Vitalik Pasemko <88082270+VitalikPasemko@users.noreply.github.com>
2021-10-04 20:00:17 +0300

    Update day-11.js

--- day-11.js
+++ day-11.js
@@ -11,3 +11,12 @@ function notEqual(s1, s2) {
   if (arr1.length !== arr2.length) return true;
   return !arr1.every((v, i) => v === arr2[i]);
 }
+
+//https://www.codewars.com/kata/5609fd5b44e602b2ff00003a/train/javascript
+function process2Arrays(arr1, arr2) {
+  let c1 = [...new Set(arr1)].filter((v) => arr2.includes(v)).length;
+  let c3 = [...new Set(arr1)].filter((v) => !arr2.includes(v)).length;
+  let c4 = [...new Set(arr2)].filter((v) => !arr1.includes(v)).length;
+  let c2 = c3 + c4;
+  return [c1, c2, c3, c4];
+}
