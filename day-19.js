3573a0344fae3bd44229942b600d60cd76ec72af
Vitalik Pasemko <88082270+VitalikPasemko@users.noreply.github.com>
2021-10-10 21:45:45 +0300

    Update day-19.js

--- day-19.js
+++ day-19.js
@@ -1,1 +1,17 @@
+//https://www.codewars.com/kata/5735e39313c205fe39001173/train/javascript
+function countAnimals(animals, count) {
+  let res = [];
+  for (let i = 0; i < count.length; i++)
+    res.push(animals.split(count[i]).length - 1);
+  return res;
+}
 
+https://www.codewars.com/kata/573975d3ac3eec695b0013e0/train/javascript
+function findSimilarity(str, word) {
+  const a = word[0] + word.slice(1, -1).replace(/./g, ".") + word.slice(-1);
+  const b = new RegExp("^" + a + "$");
+  return str
+    .split(" ")
+    .filter((v) => b.test(v))
+    .join(" ");
+}
