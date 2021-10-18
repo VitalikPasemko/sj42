813044dbc34ee08ac68752c93bbf39e44a56c8dd
Vitalik Pasemko <88082270+VitalikPasemko@users.noreply.github.com>
2021-10-18 16:24:40 +0300

    Create day-27.js

---
+++ day-27.js
@@ -0,0 +1,21 @@
+//https://www.codewars.com/kata/56b71b1dbd06e6d88100092a/train/javascript
+function buildFun(n) {
+  const res = [];
+  for (let i = 0; i < n; i++) {
+    res.push(function () {
+      return i;
+    });
+  }
+  return res;
+}
+
+//https://www.codewars.com/kata/shifty-closures/train/javascript
+let name;
+const greet_abe = function() {
+  name = 'Abe';
+  return "Hello, " + name + '!';
+};
+const greet_ben = function() {
+  name = 'Ben'; 
+  return "Hello, " + name + '!';
+};
