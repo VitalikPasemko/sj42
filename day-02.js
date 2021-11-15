3627c513952ebd7fe4c1feba8db64c204ac7ee99
Vitalik Pasemko <88082270+VitalikPasemko@users.noreply.github.com>
2021-11-15 21:18:22 +0200

    Update day-02.js

--- day-02.js
+++ day-02.js
@@ -84,6 +84,6 @@ function popElement(arr) {
 }
 
 //https://www.codewars.com/kata/571f1eb77e8954a812000837/train/javascript
-function animal(obj){     
-  return 'This ' + obj.color + ' ' + obj.name + ' has ' + obj.legs + ' legs.';
+function animal(obj) {
+  return "This " + obj.color + " " + obj.name + " has " + obj.legs + " legs.";
 }
