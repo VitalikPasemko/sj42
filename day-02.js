e44462b4e156dc1a6db6fc00b00bee70a12deac3
Vitalik Pasemko <88082270+VitalikPasemko@users.noreply.github.com>
2021-11-15 20:48:08 +0200

    Update day-02.js

--- day-02.js
+++ day-02.js
@@ -1,57 +1,68 @@
 //https://www.codewars.com/kata/571edd157e8954bab500032d/train/javascript
-const v1 = 50,
+let v1 = 50,
   v2 = 100,
   v3 = 150,
   v4 = 200,
   v5 = 2,
   v6 = 250;
 function equal1() {
-  let a = v1;
-  let b = v1;
+  const a = v1;
+  const b = v1;
   return a + b;
 }
 function equal2() {
-  let a = v4;
-  let b = v2;
+  const a = v4;
+  const b = v2;
   return a - b;
 }
 function equal3() {
-  let a = v1;
-  let b = v5;
+  const a = v1;
+  const b = v5;
   return a * b;
 }
 function equal4() {
-  let a = v4;
-  let b = v5;
+  const a = v4;
+  const b = v5;
   return a / b;
 }
 function equal5() {
-  let a = v6;
-  let b = v3;
+  const a = v6;
+  const b = v3;
   return a % b;
 }
 
 //https://www.codewars.com/kata/571edea4b625edcb51000d8e/train/javascript
-var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
-function Dad(){
-  return d1+a2+d2;
+let a1 = "A",
+  a2 = "a",
+  b1 = "B",
+  b2 = "b",
+  c1 = "C",
+  c2 = "c",
+  d1 = "D",
+  d2 = "d",
+  e1 = "E",
+  e2 = "e",
+  n1 = "N",
+  n2 = "n";
+function Dad() {
+  return d1 + a2 + d2;
 }
-function Bee(){
-  return b1+e2+e2;
+function Bee() {
+  return b1 + e2 + e2;
 }
-function banana(){
-  return b2+a2+n2+a2+n2+a2;
+function banana() {
+  return b2 + a2 + n2 + a2 + n2 + a2;
 }
-
-function answer1(){
+function answer1() {
   return "no";
 }
-function answer2(){
+function answer2() { 
   return "no";
 }
-function answer3(){
+function answer3() {
   return "yes";
 }
+
 
 //https://www.codewars.com/kata/571effabb625ed9b0600107a/train/javascript
 function getLength(arr){
