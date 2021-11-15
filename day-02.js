83d94771661e960297c5d05872db72f51cf230e5
Vitalik Pasemko <88082270+VitalikPasemko@users.noreply.github.com>
2021-11-15 20:38:12 +0200

    Update day-02.js

--- day-02.js
+++ day-02.js
@@ -1,30 +1,34 @@
 //https://www.codewars.com/kata/571edd157e8954bab500032d/train/javascript
-
-var v1=50;v2=100,v3=150,v4=200,v5=2,v6=250
-function equal1(){
-  var a=v1   
-  var b=v1   
-  return a+b;
+const v1 = 50,
+  v2 = 100,
+  v3 = 150,
+  v4 = 200,
+  v5 = 2,
+  v6 = 250;
+function equal1() {
+  let a = v1;
+  let b = v1;
+  return a + b;
 }
-function equal2(){
-  var a=v4  
-  var b=v2   
-  return a-b;
+function equal2() {
+  let a = v4;
+  let b = v2;
+  return a - b;
 }
-function equal3(){
-  var a=v1   
-  var b=v5   
-  return a*b;
+function equal3() {
+  let a = v1;
+  let b = v5;
+  return a * b;
 }
-function equal4(){
-  var a=v4   
-  var b=v5   
-  return a/b;
+function equal4() {
+  let a = v4;
+  let b = v5;
+  return a / b;
 }
-function equal5(){
-  var a=v6  
-  var b=v3   
-  return a%b;
+function equal5() {
+  let a = v6;
+  let b = v3;
+  return a % b;
 }
 
 //https://www.codewars.com/kata/571edea4b625edcb51000d8e/train/javascript
